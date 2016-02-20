ed86d6e47015ea9554d59657f70cdf39b6101213
Jamaloso <c054seis>
2016-02-20 08:46:59 +0100

    dbhelper constructor

--- app/src/main/java/com/example/abhinav/sahaya/DbHelper.java
+++ app/src/main/java/com/example/abhinav/sahaya/DbHelper.java
@@ -1,5 +1,6 @@
 package com.example.abhinav.sahaya;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -7,6 +8,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Xavier Marquès on 20/02/16.
  */
 public class DbHelper extends SQLiteOpenHelper {
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS " + Constants.TABLE_NAME + " ( pillID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, hour INTEGER, minute INTEGER, day_start INTEGER, month_start INTEGER, year_start INTEGER " +
